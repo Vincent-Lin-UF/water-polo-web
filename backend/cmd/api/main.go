4bc9dec7a8750c2b5b375fb3d0dafd9eaf9d9478
Vincent-Lin-UF <vincentlin1617@gmail.com>
2025-06-04 03:29:18 -0400

    added basic Gin server with health check

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/vincent-lin-uf/water-polo-web/backend/configs"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	cfg := configs.Load()
+
+	router := gin.Default()
+
+	// Health check endpoint
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "ok"})
+	})
+
+	log.Printf("Starting server on port %s\n", cfg.PORT)
+	if err := router.Run(":" + cfg.PORT); err != nil {
+		log.Fatal("Failed to run server:", err)
+	}
+}
